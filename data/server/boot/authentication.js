c48a7d79d357a22eacdb03057cfee240c94eda47
Richard Shi <kmshi_array@yahoo.com>
2018-06-08 00:43:03 +0800

    use basic-auth and loopback-connector-rest, perfect

--- data/server/boot/authentication.js
+++ data/server/boot/authentication.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function (app) {
-    app.enableAuth();
+    //app.enableAuth();
 };
--- data/server/boot/root.js
+++ data/server/boot/root.js
@@ -1,8 +1,15 @@
 'use strict';
+const basicAuth = require('express-basic-auth')
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
+
+  server.use(basicAuth({
+	  users: { 'admin': 'supersecret' },
+	  challenge: true,
+      realm: 'Imb4T3st4pp'
+  }));
 };
