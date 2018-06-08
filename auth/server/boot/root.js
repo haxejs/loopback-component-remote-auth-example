8e8acb470e868c8a77111f93a33efdd2de66a9a2
Richard Shi <kmshi_array@yahoo.com>
2018-06-08 23:25:01 +0800

    RestResource and template functions can not exist at same time

--- auth/server/boot/root.js
+++ auth/server/boot/root.js
@@ -5,4 +5,11 @@ module.exports = function (server) {
     var router = server.loopback.Router();
     router.get('/', server.loopback.status());
     server.use(router);
+
+    server.models.accountService.count(process.env.ACCOUNT_SERVICE_BASEURL+'/agendaJobs').then(res=>{console.log('promise count',res);});
+
+    server.models.accountService.count(process.env.ACCOUNT_SERVICE_BASEURL+'/agendaJobs',(err,body,resp)=>{
+    	console.log('callback body',body);
+    	//console.log('callback resp',resp);
+    });
 };
